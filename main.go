3b2e0c27b3b82e65ee2a0e150fe9cf5cb57a6cc8
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2014-12-13 14:32:34 +0700

    versioning

--- main.go
+++ main.go
@@ -4,9 +4,24 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/ping", pongV1)
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/ping", pongV2)
+	}
 
 	r.Run(":8080")
+}
+
+func pongV1(c *gin.Context) {
+	c.String(200, "pong")
+}
+
+func pongV2(c *gin.Context) {
+	c.String(200, "pongpong")
 }
